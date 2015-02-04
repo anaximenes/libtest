6681bd1d2f658ff4fca817de270d77fb40b2bbe7
anaximenes <yanaximenes@yandex.ru>
2015-02-05 00:48:12 +0300

    nofavorites, norecent layout changed

--- js/modules/nls/en-us/norecent.js
+++ js/modules/nls/en-us/norecent.js
@@ -2,7 +2,7 @@ define({
   'label_h1_a': 'You can always find books you ',
   'label_read': 'read',
   'label_h1_b': ' here.',
-  'label_small': 'Only works when you are logged in!',
-  'label_bottom': "And don't forget to",
+  'label_small': 'Only works when you are logged in, though.',
+  'label_bottom': "So don't forget to",
   'label_or': 'or'
 })
