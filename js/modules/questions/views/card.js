b0c3ebe3d6f0a682e4201c70ffb4615ef5f4a8a6
Alexander <alex.kostitsyn@gmail.com>
2015-02-23 21:23:39 +0300

    init

--- js/modules/questions/views/card.js
+++ js/modules/questions/views/card.js
@@ -2,19 +2,30 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!/templates/questions/questions-card.html'
+    'text!/templates/questions/questions-card.html',
+    'modules/post/main'
   ],
-  function($, _, Backbone, Template) {
+  function($, _, Backbone, Template, Post) {
     var CardItemView = Backbone.View.extend({
+      answerForm: null,
+      
       render: function() {
         if (this.model.complete()) {
-          this.$el.html(_.template(Template)(this.model.present()))
+          this.$el.html(_.template(Template)(this.model.present()));
+          this.answerForm.$el = this.$el.find('.answerForm').first();
+          this.answerForm.render();
         }
-        return this
+        return this;
       },
 
       initialize: function() {
-        this.listenTo(this.model, 'change', this.render)
+        this.listenTo(this.model, 'change', this.render);
+
+        this.answerForm = new Post.UserPostView({
+          id: this.model.id,
+          show: true
+        });
+
       }
     })
 
